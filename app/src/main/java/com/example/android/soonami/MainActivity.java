97cccc42b320343ca578d8dc4dc099ff18d65b08
Muir Halleron <muir.halleron@gmail.com>
2017-05-25 11:39:14 +0100

    Add Log Errors code

--- app/src/main/java/com/example/android/soonami/MainActivity.java
+++ app/src/main/java/com/example/android/soonami/MainActivity.java
@@ -113,7 +113,8 @@ public class MainActivity extends AppCompatActivity {
             try {
                 jsonResponse = makeHttpRequest(url);
             } catch (IOException e) {
-                // TODO Handle the IOException
+                // Handle the IOException
+                Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
             }
 
             // Extract relevant fields from the JSON response and create an {@link Event} object
@@ -175,10 +176,13 @@ public class MainActivity extends AppCompatActivity {
                 if (urlConnection.getResponseCode() == 200){
                     inputStream = urlConnection.getInputStream();
                     jsonResponse = readFromStream(inputStream);
-                }
+                }else
+                    Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+
 
             } catch (IOException e) {
-                // TODO: Handle the exception
+                // Handle the exception
+                Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
             } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
@@ -218,7 +222,7 @@ public class MainActivity extends AppCompatActivity {
             if (TextUtils.isEmpty(earthquakeJSON)){
                 return null;
             }
-            
+
             try {
                 JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
                 JSONArray featureArray = baseJsonResponse.getJSONArray("features");
